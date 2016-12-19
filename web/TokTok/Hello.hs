b163aed23176300b6da2ada9cf2d5e651ce9c369
iphydf <iphydf@users.noreply.github.com>
2016-12-19 23:17:36 +0000

    Add /hello/pulls html output for open PRs (hub-pulls).

--- web/TokTok/Hello.hs
+++ web/TokTok/Hello.hs
@@ -1,35 +1,46 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PolyKinds         #-}
-{-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 module TokTok.Hello (newApp) where
 
-import           Control.Applicative ((<$>), (<*>))
-import           Control.Monad.Trans (lift)
-import           Data.Aeson          (FromJSON, ToJSON)
-import           Data.IORef          (IORef)
-import qualified Data.IORef          as IORef
-import           Data.Monoid         ((<>))
-import           Data.Text           (Text, toUpper)
-import           GHC.Generics        (Generic)
-import           Network.Wai         (Application)
+import           Control.Applicative   ((<$>), (<*>))
+import           Data.Aeson            (FromJSON, ToJSON)
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy  as LBS
+import           Data.Monoid           ((<>))
+import           Data.Text             (Text)
+import qualified Data.Text             as Text
+import           Data.Text.Encoding    (encodeUtf8)
+import           GHC.Generics          (Generic)
+import qualified GitHub
+import           Network.HTTP.Media    ((//), (/:))
+import           Network.Wai           (Application)
 import           Servant
+import           System.Environment    (getEnv)
 
 import qualified Changelogs
+import qualified PullStatus
 
 
 data ApiContext = ApiContext
-  { getChangelog :: IORef Changelogs.ChangeLog
-  , getRoadmap   :: IORef Changelogs.ChangeLog
+  { getChangelog :: Changelogs.ChangeLog
+  , getRoadmap   :: Changelogs.ChangeLog
+  , getPulls     :: Text
   }
 
 
 newContext :: IO ApiContext
-newContext = ApiContext
-  <$> (IORef.newIORef =<< Changelogs.fetchChangeLog False "TokTok" "c-toxcore" Nothing)
-  <*> (IORef.newIORef =<< Changelogs.fetchChangeLog True  "TokTok" "c-toxcore" Nothing)
+newContext = do
+  auth <- Just . GitHub.OAuth . BS8.pack <$> getEnv "GITHUB_TOKEN"
+
+  ApiContext
+    <$> Changelogs.fetchChangeLog False "TokTok" "c-toxcore" Nothing
+    <*> Changelogs.fetchChangeLog True  "TokTok" "c-toxcore" Nothing
+    <*> (Text.pack <$> PullStatus.getPullStatus "TokTok" "TokTok" True auth)
 
 
 -- * Example
@@ -41,6 +52,12 @@ newtype Greet = Greet { _msg :: Text }
 instance FromJSON Greet
 instance ToJSON Greet
 
+data HTML
+instance Accept HTML where
+  contentType _ = "text" // "html" /: ("charset", "utf-8")
+instance MimeRender HTML Text where
+  mimeRender _ = LBS.fromStrict . encodeUtf8
+
 -- API specification
 type TestApi =
        -- GET /hello/:name?capital={true, false}  returns a Greet as JSON
@@ -48,6 +65,7 @@ type TestApi =
 
   :<|> "changelog" :> Get '[PlainText] Text
   :<|> "roadmap" :> Get '[PlainText] Text
+  :<|> "pulls" :> Get '[HTML] Text
 
        -- POST /greet with a Greet as JSON in the request body,
        --             returns a Greet as JSON
@@ -70,15 +88,18 @@ server ctx =
        helloH
   :<|> changelogH False
   :<|> changelogH True
+  :<|> pullsH
   :<|> postGreetH
   :<|> deleteGreetH
   where
     helloH name Nothing      = helloH name (Just False)
     helloH name (Just False) = return . Greet $ "Hello, " <> name
-    helloH name (Just True)  = return . Greet . toUpper $ "Hello, " <> name
+    helloH name (Just True)  = return . Greet . Text.toUpper $ "Hello, " <> name
 
-    changelogH False = lift $ Changelogs.formatChangeLog False <$> IORef.readIORef (getChangelog ctx)
-    changelogH True  = lift $ Changelogs.formatChangeLog True  <$> IORef.readIORef (getRoadmap ctx)
+    changelogH False = return $ Changelogs.formatChangeLog False (getChangelog ctx)
+    changelogH True  = return $ Changelogs.formatChangeLog True  (getRoadmap ctx)
+
+    pullsH = return $ getPulls ctx
 
     postGreetH greet = return greet
 
