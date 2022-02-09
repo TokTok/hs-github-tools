61914d5bac3af175840f773ebc7067e01c8e5888
iphydf <iphydf@users.noreply.github.com>
2022-02-09 23:55:10 +0000

    chore: Add new permissions to the data model.

--- src/GitHub/Types/Base/Permissions.hs
+++ src/GitHub/Types/Base/Permissions.hs
@@ -13,59 +13,91 @@ import           Test.QuickCheck.Arbitrary (Arbitrary (..))
 -- Permissions
 
 data Permissions = Permissions
-    { permissionsChecks            :: Maybe Text
-    , permissionsContents          :: Maybe Text
-    , permissionsContentReferences :: Maybe Text
-    , permissionsDeployments       :: Maybe Text
-    , permissionsIssues            :: Maybe Text
-    , permissionsMembers           :: Maybe Text
-    , permissionsMetadata          :: Maybe Text
-    , permissionsPages             :: Maybe Text
-    , permissionsPullRequests      :: Maybe Text
-    , permissionsRepositoryHooks   :: Maybe Text
-    , permissionsSingleFile        :: Maybe Text
-    , permissionsStatuses          :: Maybe Text
+    { permissionsActions              :: Maybe Text
+    , permissionsAdministration       :: Maybe Text
+    , permissionsChecks               :: Maybe Text
+    , permissionsContents             :: Maybe Text
+    , permissionsContentReferences    :: Maybe Text
+    , permissionsDeployments          :: Maybe Text
+    , permissionsDiscussions          :: Maybe Text
+    , permissionsIssues               :: Maybe Text
+    , permissionsMembers              :: Maybe Text
+    , permissionsMetadata             :: Maybe Text
+    , permissionsOrganizationPackages :: Maybe Text
+    , permissionsPackages             :: Maybe Text
+    , permissionsPages                :: Maybe Text
+    , permissionsPullRequests         :: Maybe Text
+    , permissionsRepositoryHooks      :: Maybe Text
+    , permissionsRepositoryProjects   :: Maybe Text
+    , permissionsSecurityEvents       :: Maybe Text
+    , permissionsSingleFile           :: Maybe Text
+    , permissionsStatuses             :: Maybe Text
+    , permissionsVulnerabilityAlerts  :: Maybe Text
     } deriving (Eq, Show, Read)
 
 
 instance FromJSON Permissions where
     parseJSON (Object x) = Permissions
-        <$> x .:? "checks"
+        <$> x .:? "actions"
+        <*> x .:? "administration"
+        <*> x .:? "checks"
         <*> x .:? "contents"
         <*> x .:? "content_references"
         <*> x .:? "deployments"
+        <*> x .:? "discussions"
         <*> x .:? "issues"
         <*> x .:? "members"
         <*> x .:? "metadata"
+        <*> x .:? "organization_packages"
+        <*> x .:? "packages"
         <*> x .:? "pages"
         <*> x .:? "pull_requests"
         <*> x .:? "repository_hooks"
+        <*> x .:? "repository_projects"
+        <*> x .:? "security_events"
         <*> x .:? "single_file"
         <*> x .:? "statuses"
+        <*> x .:? "vulnerability_alerts"
 
     parseJSON _ = fail "Permissions"
 
 
 instance ToJSON Permissions where
     toJSON Permissions{..} = object
-        [ "checks"             .= permissionsChecks
-        , "contents"           .= permissionsContents
-        , "content_references" .= permissionsContentReferences
-        , "deployments"        .= permissionsDeployments
-        , "issues"             .= permissionsIssues
-        , "members"            .= permissionsMembers
-        , "metadata"           .= permissionsMetadata
-        , "pages"              .= permissionsPages
-        , "pull_requests"      .= permissionsPullRequests
-        , "repository_hooks"   .= permissionsRepositoryHooks
-        , "single_file"        .= permissionsSingleFile
-        , "statuses"           .= permissionsStatuses
+        [ "actions"               .= permissionsActions
+        , "administration"        .= permissionsAdministration
+        , "checks"                .= permissionsChecks
+        , "contents"              .= permissionsContents
+        , "content_references"    .= permissionsContentReferences
+        , "deployments"           .= permissionsDeployments
+        , "discussions"           .= permissionsDiscussions
+        , "issues"                .= permissionsIssues
+        , "members"               .= permissionsMembers
+        , "metadata"              .= permissionsMetadata
+        , "organization_packages" .= permissionsOrganizationPackages
+        , "packages"              .= permissionsPackages
+        , "pages"                 .= permissionsPages
+        , "pull_requests"         .= permissionsPullRequests
+        , "repository_hooks"      .= permissionsRepositoryHooks
+        , "repository_projects"   .= permissionsRepositoryProjects
+        , "security_events"       .= permissionsSecurityEvents
+        , "single_file"           .= permissionsSingleFile
+        , "statuses"              .= permissionsStatuses
+        , "vulnerability_alerts"  .= permissionsVulnerabilityAlerts
         ]
 
 
 instance Arbitrary Permissions where
     arbitrary = Permissions
         <$> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
         <*> arbitrary
         <*> arbitrary
         <*> arbitrary
