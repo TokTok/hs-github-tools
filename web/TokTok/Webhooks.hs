8556d67be920fc8981f2d62ff4c8628d4c1c4984
iphydf <iphydf@users.noreply.github.com>
2022-02-11 14:33:27 +0000

    perf: Use background processing for automerge.
    This limits the amount of work done synchronously in a request and
    collapses multiple checks keyed by "repo/author" into one, every 10
    seconds. The timer is reset to 10 seconds when a new request comes in.

--- web/TokTok/Webhooks.hs
+++ web/TokTok/Webhooks.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
-module TokTok.Webhooks (app) where
+module TokTok.Webhooks (newApp) where
 
+import           Control.Concurrent.MVar    (MVar, modifyMVar_, newMVar,
+                                             swapMVar)
+import           Control.Concurrent.Suspend (sDelay)
+import           Control.Concurrent.Timer   (TimerIO, oneShotRestart,
+                                             oneShotTimer)
 import qualified Data.Aeson.Encode.Pretty   as Aeson
 import qualified Data.Algorithm.DiffContext as Diff
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Lazy       as LBS
 import qualified Data.CaseInsensitive       as CI
+import           Data.Map                   (Map)
+import qualified Data.Map                   as Map
 import qualified Data.Maybe                 as Maybe
 import           Data.Text                  (Text)
 import qualified Data.Text                  as Text
@@ -76,21 +83,20 @@ handleError event headers err = do
     return . responseError . LBS.fromStrict . Text.encodeUtf8 $ res
 
 
-handlePayload :: Bool -> BS.ByteString -> UUID -> Payload -> IO Response
-handlePayload isSigned event uuid payload = do
+handlePayload :: (Payload -> IO Response) -> Bool -> BS.ByteString -> UUID -> Payload -> IO Response
+handlePayload f isSigned event uuid payload = do
     Text.putStrLn
         $  unsignedMsg
         <> "Success in event "
         <> Text.pack (show event)
         <> ": UUID="
         <> Text.pack (show uuid)
-    handleEvent payload
-    return responseOK
+    f payload
     where unsignedMsg = if isSigned then "" else "[UNSIGNED!] "
 
 
-handleRequest :: BS.ByteString -> [String] -> Request -> IO Response
-handleRequest event secretKeys req = do
+handleRequest :: (Payload -> IO Response) -> BS.ByteString -> [String] -> Request -> IO Response
+handleRequest f event secretKeys req = do
     body   <- fullRequestBody mempty
     parsed <- runHandler Handler
         { hSecretKeys = secretKeys
@@ -100,7 +106,7 @@ handleRequest event secretKeys req = do
     case parsed of
         Left err -> handleError event (requestHeaders req) err
         Right (uuid, payload) ->
-            handlePayload (not $ null secretKeys) event uuid payload
+            handlePayload f (not $ null secretKeys) event uuid payload
 
   where
     fullRequestBody body = do
@@ -108,14 +114,39 @@ handleRequest event secretKeys req = do
         if BS.null chunk then return body else fullRequestBody $ body <> chunk
 
 
-app :: Application
-app req respond = case lookup "X-GitHub-Event" . requestHeaders $ req of
+app :: (Payload -> IO Response) -> Application
+app f req respond = case lookup "X-GitHub-Event" . requestHeaders $ req of
     Nothing -> do
-      -- Respond OK if we don't get an event.
+        -- Respond OK if we don't get an event.
         putStrLn "Ignoring request without event type"
         respond responseOK
     Just event -> do
         putStrLn $ "Handling GitHub event: " ++ show event
         -- Get the github secret keys.
         secretKeys <- Maybe.maybeToList <$> lookupEnv "GITHUB_SECRET"
-        respond =<< handleRequest event secretKeys req
+        respond =<< handleRequest f event secretKeys req
+
+newApp :: IO Application
+newApp = do
+    -- Initialise task queue
+    tasks <- newMVar Map.empty
+    -- Start the task queue processing timer.
+    timer <- oneShotTimer (processTasks tasks) (sDelay 10)  -- after 10 seconds
+    return $ app (addTasks timer tasks)
+  where
+    processTasks :: MVar (Map Text (IO ())) -> IO ()
+    processTasks tasks = do
+        todo <- swapMVar tasks Map.empty
+        putStrLn $ "processing " <> show (Map.size todo) <> " background tasks"
+        sequence_ $ Map.elems todo
+
+    addTasks :: TimerIO -> MVar (Map Text (IO ())) -> Payload -> IO Response
+    addTasks timer tasks payload =
+        case handleEvent payload of
+            Nothing -> return responseOK
+            Just (key, task) -> do
+                modifyMVar_ tasks $ return . Map.insert key task
+                success <- oneShotRestart timer
+                return $ if success
+                    then responseOK
+                    else responseError "failed to start background processing task"
