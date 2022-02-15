8af5095e629d9a05af37bda5599acf373c2690ad
Restyled.io <commits@restyled.io>
2022-02-15 19:43:52 +0000

    Restyled by shellharden

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -8,12 +8,12 @@ set -eux
 APP=git-critique
 PKG=github-tools
 
-DIR=$(realpath $(dirname "$0")/..)
+DIR=$(realpath "$(dirname "$0")"/..)
 cd "$DIR"
 docker build \
   -t "toxchat/haskell:hs-$PKG" \
   -f ".github/docker/Dockerfile" \
-  - < <(tar c LICENSE stack.yaml "$PKG.cabal" $(find . -name "*.hs" -or -name "Dockerfile*"))
+  - < <(tar c LICENSE stack.yaml "$PKG.cabal" "$(find . -name "*.hs" -or -name "Dockerfile*")")
 docker build \
   -t "registry.heroku.com/$APP/web" \
   -f ".github/docker/Dockerfile.heroku" \
