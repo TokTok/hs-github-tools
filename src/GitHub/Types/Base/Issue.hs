0a529d20a5e2e68844b7f61d069514a9b82bee28
iphydf <iphydf@users.noreply.github.com>
2022-02-16 00:56:36 +0000

    feat: Add support for issue comment reactions.

--- src/GitHub/Types/Base/Issue.hs
+++ src/GitHub/Types/Base/Issue.hs
@@ -14,42 +14,49 @@ import           GitHub.Types.Base.DateTime
 import           GitHub.Types.Base.Label
 import           GitHub.Types.Base.Milestone
 import           GitHub.Types.Base.PullRequestRef
+import           GitHub.Types.Base.Reactions
 import           GitHub.Types.Base.User
 
 ------------------------------------------------------------------------------
 -- Issue
 
 data Issue = Issue
-    { issueAssignee          :: Maybe User
-    , issueAssignees         :: [User]
-    , issueAuthorAssociation :: Text
-    , issueBody              :: Text
-    , issueClosedAt          :: Maybe DateTime
-    , issueComments          :: Int
-    , issueCommentsUrl       :: Text
-    , issueCreatedAt         :: DateTime
-    , issueEventsUrl         :: Text
-    , issueHtmlUrl           :: Text
-    , issueId                :: Int
-    , issueLabels            :: [Label]
-    , issueLabelsUrl         :: Text
-    , issueLocked            :: Bool
-    , issueMilestone         :: Maybe Milestone
-    , issueNodeId            :: Text
-    , issueNumber            :: Int
-    , issuePullRequest       :: Maybe PullRequestRef
-    , issueRepositoryUrl     :: Text
-    , issueState             :: Text
-    , issueTitle             :: Text
-    , issueUpdatedAt         :: DateTime
-    , issueUrl               :: Text
-    , issueUser              :: User
+    { issueActiveLockReason      :: Maybe Text
+    , issueAssignee              :: Maybe User
+    , issueAssignees             :: [User]
+    , issueAuthorAssociation     :: Text
+    , issueBody                  :: Text
+    , issueClosedAt              :: Maybe DateTime
+    , issueComments              :: Int
+    , issueCommentsUrl           :: Text
+    , issueCreatedAt             :: DateTime
+    , issueDraft                 :: Bool
+    , issueEventsUrl             :: Text
+    , issueHtmlUrl               :: Text
+    , issueId                    :: Int
+    , issueLabels                :: [Label]
+    , issueLabelsUrl             :: Text
+    , issueLocked                :: Bool
+    , issueMilestone             :: Maybe Milestone
+    , issueNodeId                :: Text
+    , issueNumber                :: Int
+    , issuePerformedViaGithubApp :: Maybe Text
+    , issuePullRequest           :: Maybe PullRequestRef
+    , issueReactions             :: Reactions
+    , issueRepositoryUrl         :: Text
+    , issueState                 :: Text
+    , issueTimelineUrl           :: Text
+    , issueTitle                 :: Text
+    , issueUpdatedAt             :: DateTime
+    , issueUrl                   :: Text
+    , issueUser                  :: User
     } deriving (Eq, Show, Read)
 
 
 instance FromJSON Issue where
     parseJSON (Object x) = Issue
-        <$> x .: "assignee"
+        <$> x .: "active_lock_reason"
+        <*> x .: "assignee"
         <*> x .: "assignees"
         <*> x .: "author_association"
         <*> x .: "body"
@@ -57,6 +64,7 @@ instance FromJSON Issue where
         <*> x .: "comments"
         <*> x .: "comments_url"
         <*> x .: "created_at"
+        <*> x .: "draft"
         <*> x .: "events_url"
         <*> x .: "html_url"
         <*> x .: "id"
@@ -66,9 +74,12 @@ instance FromJSON Issue where
         <*> x .: "milestone"
         <*> x .: "node_id"
         <*> x .: "number"
+        <*> x .: "performed_via_github_app"
         <*> x .:? "pull_request"
+        <*> x .: "reactions"
         <*> x .: "repository_url"
         <*> x .: "state"
+        <*> x .: "timeline_url"
         <*> x .: "title"
         <*> x .: "updated_at"
         <*> x .: "url"
@@ -79,36 +90,46 @@ instance FromJSON Issue where
 
 instance ToJSON Issue where
     toJSON Issue{..} = object
-        [ "assignee"           .= issueAssignee
-        , "assignees"          .= issueAssignees
-        , "author_association" .= issueAuthorAssociation
-        , "body"               .= issueBody
-        , "closed_at"          .= issueClosedAt
-        , "comments"           .= issueComments
-        , "comments_url"       .= issueCommentsUrl
-        , "created_at"         .= issueCreatedAt
-        , "events_url"         .= issueEventsUrl
-        , "html_url"           .= issueHtmlUrl
-        , "id"                 .= issueId
-        , "labels"             .= issueLabels
-        , "labels_url"         .= issueLabelsUrl
-        , "locked"             .= issueLocked
-        , "milestone"          .= issueMilestone
-        , "node_id"            .= issueNodeId
-        , "number"             .= issueNumber
-        , "pull_request"       .= issuePullRequest
-        , "repository_url"     .= issueRepositoryUrl
-        , "state"              .= issueState
-        , "title"              .= issueTitle
-        , "updated_at"         .= issueUpdatedAt
-        , "url"                .= issueUrl
-        , "user"               .= issueUser
+        [ "active_lock_reason"       .= issueActiveLockReason
+        , "assignee"                 .= issueAssignee
+        , "assignees"                .= issueAssignees
+        , "author_association"       .= issueAuthorAssociation
+        , "body"                     .= issueBody
+        , "closed_at"                .= issueClosedAt
+        , "comments"                 .= issueComments
+        , "comments_url"             .= issueCommentsUrl
+        , "created_at"               .= issueCreatedAt
+        , "draft"                    .= issueDraft
+        , "events_url"               .= issueEventsUrl
+        , "html_url"                 .= issueHtmlUrl
+        , "id"                       .= issueId
+        , "labels"                   .= issueLabels
+        , "labels_url"               .= issueLabelsUrl
+        , "locked"                   .= issueLocked
+        , "milestone"                .= issueMilestone
+        , "node_id"                  .= issueNodeId
+        , "number"                   .= issueNumber
+        , "performed_via_github_app" .= issuePerformedViaGithubApp
+        , "pull_request"             .= issuePullRequest
+        , "reactions"                .= issueReactions
+        , "repository_url"           .= issueRepositoryUrl
+        , "state"                    .= issueState
+        , "timeline_url"             .= issueTimelineUrl
+        , "title"                    .= issueTitle
+        , "updated_at"               .= issueUpdatedAt
+        , "url"                      .= issueUrl
+        , "user"                     .= issueUser
         ]
 
 
 instance Arbitrary Issue where
     arbitrary = Issue
         <$> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
         <*> arbitrary
         <*> arbitrary
         <*> arbitrary
--- src/GitHub/Types/Base/IssueComment.hs
+++ src/GitHub/Types/Base/IssueComment.hs
@@ -3,28 +3,32 @@
 {-# LANGUAGE StrictData        #-}
 module GitHub.Types.Base.IssueComment where
 
-import           Data.Aeson                 (FromJSON (..), ToJSON (..), object)
-import           Data.Aeson.Types           (Value (..), (.:), (.=))
-import           Data.Text                  (Text)
-import           Test.QuickCheck.Arbitrary  (Arbitrary (..))
+import           Data.Aeson                  (FromJSON (..), ToJSON (..),
+                                              object)
+import           Data.Aeson.Types            (Value (..), (.:), (.=))
+import           Data.Text                   (Text)
+import           Test.QuickCheck.Arbitrary   (Arbitrary (..))
 
 import           GitHub.Types.Base.DateTime
+import           GitHub.Types.Base.Reactions
 import           GitHub.Types.Base.User
 
 ------------------------------------------------------------------------------
 -- IssueComment
 
 data IssueComment = IssueComment
-    { issueCommentAuthorAssociation :: Text
-    , issueCommentBody              :: Text
-    , issueCommentCreatedAt         :: DateTime
-    , issueCommentHtmlUrl           :: Text
-    , issueCommentId                :: Int
-    , issueCommentNodeId            :: Text
-    , issueCommentIssueUrl          :: Text
-    , issueCommentUpdatedAt         :: DateTime
-    , issueCommentUrl               :: Text
-    , issueCommentUser              :: User
+    { issueCommentAuthorAssociation     :: Text
+    , issueCommentBody                  :: Text
+    , issueCommentCreatedAt             :: DateTime
+    , issueCommentHtmlUrl               :: Text
+    , issueCommentId                    :: Int
+    , issueCommentNodeId                :: Text
+    , issueCommentIssueUrl              :: Text
+    , issueCommentPerformedViaGithubApp :: Maybe Bool
+    , issueCommentReactions             :: Reactions
+    , issueCommentUpdatedAt             :: DateTime
+    , issueCommentUrl                   :: Text
+    , issueCommentUser                  :: User
     } deriving (Eq, Show, Read)
 
 
@@ -37,6 +41,8 @@ instance FromJSON IssueComment where
         <*> x .: "id"
         <*> x .: "node_id"
         <*> x .: "issue_url"
+        <*> x .: "performed_via_github_app"
+        <*> x .: "reactions"
         <*> x .: "updated_at"
         <*> x .: "url"
         <*> x .: "user"
@@ -46,22 +52,26 @@ instance FromJSON IssueComment where
 
 instance ToJSON IssueComment where
     toJSON IssueComment{..} = object
-        [ "author_association" .= issueCommentAuthorAssociation
-        , "body"               .= issueCommentBody
-        , "created_at"         .= issueCommentCreatedAt
-        , "html_url"           .= issueCommentHtmlUrl
-        , "id"                 .= issueCommentId
-        , "node_id"            .= issueCommentNodeId
-        , "issue_url"          .= issueCommentIssueUrl
-        , "updated_at"         .= issueCommentUpdatedAt
-        , "url"                .= issueCommentUrl
-        , "user"               .= issueCommentUser
+        [ "author_association"       .= issueCommentAuthorAssociation
+        , "body"                     .= issueCommentBody
+        , "created_at"               .= issueCommentCreatedAt
+        , "html_url"                 .= issueCommentHtmlUrl
+        , "id"                       .= issueCommentId
+        , "node_id"                  .= issueCommentNodeId
+        , "issue_url"                .= issueCommentIssueUrl
+        , "performed_via_github_app" .= issueCommentPerformedViaGithubApp
+        , "reactions"                .= issueCommentReactions
+        , "updated_at"               .= issueCommentUpdatedAt
+        , "url"                      .= issueCommentUrl
+        , "user"                     .= issueCommentUser
         ]
 
 
 instance Arbitrary IssueComment where
     arbitrary = IssueComment
         <$> arbitrary
+        <*> arbitrary
+        <*> arbitrary
         <*> arbitrary
         <*> arbitrary
         <*> arbitrary
