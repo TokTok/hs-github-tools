37ac9fe919ea33f85421378c3904e1bdf3b8722a
iphydf <iphydf@users.noreply.github.com>
2024-11-04 16:13:47 +0000

    chore: Add more missing permissions.

--- src/GitHub/Types/Base/Permissions.hs
+++ src/GitHub/Types/Base/Permissions.hs
@@ -13,32 +13,35 @@ import           Test.QuickCheck.Arbitrary (Arbitrary (..))
 -- Permissions
 
 data Permissions = Permissions
-    { permissionsActions              :: Maybe Text
-    , permissionsAdministration       :: Maybe Text
-    , permissionsAttestations         :: Maybe Text
-    , permissionsChecks               :: Maybe Text
-    , permissionsContents             :: Maybe Text
-    , permissionsContentReferences    :: Maybe Text
-    , permissionsDeployments          :: Maybe Text
-    , permissionsDiscussions          :: Maybe Text
-    , permissionsEmails               :: Maybe Text
-    , permissionsIssues               :: Maybe Text
-    , permissionsKeys                 :: Maybe Text
-    , permissionsMembers              :: Maybe Text
-    , permissionsMergeQueues          :: Maybe Text
-    , permissionsMetadata             :: Maybe Text
-    , permissionsOrganizationHooks    :: Maybe Text
-    , permissionsOrganizationPackages :: Maybe Text
-    , permissionsOrganizationProjects :: Maybe Text
-    , permissionsPackages             :: Maybe Text
-    , permissionsPages                :: Maybe Text
-    , permissionsPullRequests         :: Maybe Text
-    , permissionsRepositoryHooks      :: Maybe Text
-    , permissionsRepositoryProjects   :: Maybe Text
-    , permissionsSecurityEvents       :: Maybe Text
-    , permissionsSingleFile           :: Maybe Text
-    , permissionsStatuses             :: Maybe Text
-    , permissionsVulnerabilityAlerts  :: Maybe Text
+    { permissionsActions                      :: Maybe Text
+    , permissionsAdministration               :: Maybe Text
+    , permissionsAttestations                 :: Maybe Text
+    , permissionsChecks                       :: Maybe Text
+    , permissionsContents                     :: Maybe Text
+    , permissionsContentReferences            :: Maybe Text
+    , permissionsDeployments                  :: Maybe Text
+    , permissionsDiscussions                  :: Maybe Text
+    , permissionsEmails                       :: Maybe Text
+    , permissionsIssues                       :: Maybe Text
+    , permissionsKeys                         :: Maybe Text
+    , permissionsMembers                      :: Maybe Text
+    , permissionsMergeQueues                  :: Maybe Text
+    , permissionsMetadata                     :: Maybe Text
+    , permissionsOrganizationAdministration   :: Maybe Text
+    , permissionsOrganizationCustomProperties :: Maybe Text
+    , permissionsOrganizationHooks            :: Maybe Text
+    , permissionsOrganizationPackages         :: Maybe Text
+    , permissionsOrganizationProjects         :: Maybe Text
+    , permissionsPackages                     :: Maybe Text
+    , permissionsPages                        :: Maybe Text
+    , permissionsPullRequests                 :: Maybe Text
+    , permissionsRepositoryCustomProperties   :: Maybe Text
+    , permissionsRepositoryHooks              :: Maybe Text
+    , permissionsRepositoryProjects           :: Maybe Text
+    , permissionsSecurityEvents               :: Maybe Text
+    , permissionsSingleFile                   :: Maybe Text
+    , permissionsStatuses                     :: Maybe Text
+    , permissionsVulnerabilityAlerts          :: Maybe Text
     } deriving (Eq, Show, Read)
 
 
@@ -58,12 +61,15 @@ instance FromJSON Permissions where
         <*> x .:? "members"
         <*> x .:? "merge_queues"
         <*> x .:? "metadata"
+        <*> x .:? "organization_administration"
+        <*> x .:? "organization_custom_properties"
         <*> x .:? "organization_hooks"
         <*> x .:? "organization_packages"
         <*> x .:? "organization_projects"
         <*> x .:? "packages"
         <*> x .:? "pages"
         <*> x .:? "pull_requests"
+        <*> x .:? "repository_custom_properties"
         <*> x .:? "repository_hooks"
         <*> x .:? "repository_projects"
         <*> x .:? "security_events"
@@ -76,38 +82,44 @@ instance FromJSON Permissions where
 
 instance ToJSON Permissions where
     toJSON Permissions{..} = object
-        [ "actions"               .= permissionsActions
-        , "administration"        .= permissionsAdministration
-        , "attestations"          .= permissionsAttestations
-        , "checks"                .= permissionsChecks
-        , "contents"              .= permissionsContents
-        , "content_references"    .= permissionsContentReferences
-        , "deployments"           .= permissionsDeployments
-        , "discussions"           .= permissionsDiscussions
-        , "emails"                .= permissionsEmails
-        , "issues"                .= permissionsIssues
-        , "keys"                  .= permissionsKeys
-        , "members"               .= permissionsMembers
-        , "merge_queues"          .= permissionsMergeQueues
-        , "metadata"              .= permissionsMetadata
-        , "organization_hooks"    .= permissionsOrganizationHooks
-        , "organization_packages" .= permissionsOrganizationPackages
-        , "organization_projects" .= permissionsOrganizationProjects
-        , "packages"              .= permissionsPackages
-        , "pages"                 .= permissionsPages
-        , "pull_requests"         .= permissionsPullRequests
-        , "repository_hooks"      .= permissionsRepositoryHooks
-        , "repository_projects"   .= permissionsRepositoryProjects
-        , "security_events"       .= permissionsSecurityEvents
-        , "single_file"           .= permissionsSingleFile
-        , "statuses"              .= permissionsStatuses
-        , "vulnerability_alerts"  .= permissionsVulnerabilityAlerts
+        [ "actions"                        .= permissionsActions
+        , "administration"                 .= permissionsAdministration
+        , "attestations"                   .= permissionsAttestations
+        , "checks"                         .= permissionsChecks
+        , "contents"                       .= permissionsContents
+        , "content_references"             .= permissionsContentReferences
+        , "deployments"                    .= permissionsDeployments
+        , "discussions"                    .= permissionsDiscussions
+        , "emails"                         .= permissionsEmails
+        , "issues"                         .= permissionsIssues
+        , "keys"                           .= permissionsKeys
+        , "members"                        .= permissionsMembers
+        , "merge_queues"                   .= permissionsMergeQueues
+        , "metadata"                       .= permissionsMetadata
+        , "organization_administration"    .= permissionsOrganizationAdministration
+        , "organization_custom_properties" .= permissionsOrganizationCustomProperties
+        , "organization_hooks"             .= permissionsOrganizationHooks
+        , "organization_packages"          .= permissionsOrganizationPackages
+        , "organization_projects"          .= permissionsOrganizationProjects
+        , "packages"                       .= permissionsPackages
+        , "pages"                          .= permissionsPages
+        , "pull_requests"                  .= permissionsPullRequests
+        , "repository_custom_properties"   .= permissionsRepositoryCustomProperties
+        , "repository_hooks"               .= permissionsRepositoryHooks
+        , "repository_projects"            .= permissionsRepositoryProjects
+        , "security_events"                .= permissionsSecurityEvents
+        , "single_file"                    .= permissionsSingleFile
+        , "statuses"                       .= permissionsStatuses
+        , "vulnerability_alerts"           .= permissionsVulnerabilityAlerts
         ]
 
 
 instance Arbitrary Permissions where
     arbitrary = Permissions
         <$> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
         <*> arbitrary
         <*> arbitrary
         <*> arbitrary
