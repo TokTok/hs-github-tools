71820fc5701dc08a776e37c217cfbc089b81e25d
iphydf <iphydf@users.noreply.github.com>
2025-02-17 23:34:33 +0000

    fix: Use `-threaded` for webservice so it crashes less.

--- web/webservice.hs
+++ web/webservice.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
@@ -6,7 +7,7 @@ import           Network.Wai                 (Application)
 import           Network.Wai.Handler.Warp    (Port, run)
 import           Network.Wai.Middleware.Cors (simpleCors)
 import           Network.Wai.UrlMap          (mapUrls, mount)
-import           System.Environment          (getArgs, getEnv)
+import           System.Environment          (getArgs, lookupEnv)
 import           System.IO                   (BufferMode (..), hSetBuffering,
                                               stdout)
 
@@ -29,12 +30,20 @@ runTestServer port = do
     putStrLn $ "Running webserver on port " ++ show port
     run port =<< newApp
 
+getPort :: IO Port
+getPort =
+    -- Prefer $PORT, otherwise the first command line argument, otherwise 8080.
+    lookupEnv "PORT" >>= \case
+        Just port -> return $ read port
+        Nothing -> do
+            args <- getArgs
+            return $ case args of
+                [port] -> read port
+                _      -> 8080
+
 -- Put this all to work!
 main :: IO ()
 main = do
-  -- So real time logging works correctly.
+    -- So real time logging works correctly.
     hSetBuffering stdout LineBuffering
-    args <- getArgs
-    case args of
-        [port] -> runTestServer $ read port
-        _      -> runTestServer =<< read <$> getEnv "PORT"
+    getPort >>= runTestServer
